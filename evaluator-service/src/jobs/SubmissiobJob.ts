d5132eb0ecad62f32caaab94b4aa567fff8b682e
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2025-08-10 16:48:33 +0530

    Fix: Refactored code using strategy pattern

--- evaluator-service/src/jobs/SubmissiobJob.ts
+++ evaluator-service/src/jobs/SubmissiobJob.ts
@@ -1,7 +1,7 @@
 import type { Job } from "bullmq";
 import type { IJob } from "../types/bullMQJobDefinition.js";
 import type { SubmissionPayload } from "../types/submissionPayload.js";
-import { runCpp } from "../containers/cppExecutor.js";
+import createExecutor from "../utils/ExecutorFactory.js";
 
 export default class SubmissionJob implements IJob {
   name: string;
@@ -17,13 +17,28 @@ export default class SubmissionJob implements IJob {
     if (job && this.payload) {
       const key = Object.keys(this.payload)[0];
       if (key && this.payload[key]) {
-        if (this.payload[key].language === "CPP") {
-          const response = await runCpp(
-            this.payload[key].code,
-            this.payload[key].inputCase
-          );
-          console.log("Evaluate response is", response);
+        const codeLanguage = this.payload[key].language;
+        const code = this.payload[key].code;
+        const inputTestCase = this.payload[key].inputCase;
+
+        const strategy = createExecutor(codeLanguage);
+        if (strategy !== null) {
+          const response = await strategy.execute(code, inputTestCase);
+          if (response.status === "COMPLETED") {
+            console.log("execution done");
+            console.log(response);
+          } else {
+            console.log("not executed");
+            console.log(response);
+          }
         }
+        // if (this.payload[key].language === "CPP") {
+        //   const response = await runCpp(
+        //     this.payload[key].code,
+        //     this.payload[key].inputCase
+        //   );
+        //   console.log("Evaluate response is", response);
+        // }
       }
     }
   };
